97d7ca1af1d4246afe94cd2919c58a9bf2557b43
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2017-11-08 00:32:23 -0700

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -2,6 +2,8 @@ add-apt-repository ppa:certbot/certbot
 apt-get update
 apt-get -y install certbot
 
-mv /etc/nginx/sites-available/default /etc/nginx/sites-available/sample
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/old
 cp /root/labzero/nginxNominal /etc/nginx/sites-available/default
 cp /root/labzero/dhparam.pem /etc/ssl/dhparam.pem
+
+servic nginx restart
