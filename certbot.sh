7dbcf38f79bf3e966f6a15470498c4501be093fb
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2017-11-08 00:00:15 -0700

    Create certbot.sh

---
+++ certbot.sh
@@ -0,0 +1,7 @@
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get -y install certbot
+
+mv /etc/nginx/sites-available/default /etc/nginx/sites-available/sample
+cp /root/labzero/nginxNominal /etc/nginx/sites-available/default
+cp /root/labzero/dhparam.pem /etc/ssl/dhparam.pem
