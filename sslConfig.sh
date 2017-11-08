1178062b24c743cc38ffc4254d818e6a76379ae5
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2017-11-08 01:03:22 -0700

    Update sslConfig.sh

--- sslConfig.sh
+++ sslConfig.sh
@@ -1,3 +1,3 @@
 # mv /etc/nginx/sites-available/default /etc/nginx/sites-available/old2
 cp /root/labzero/nginxDefaultSsl /root/testconf
-sed -i 's/MYDOMAIN.com/$(domain.txt)/g'  hello.txt
+sed -i 's/MYDOMAIN.com/$(domain.txt)/g'  testconf
