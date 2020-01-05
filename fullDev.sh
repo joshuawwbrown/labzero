83954be2800f67f1b9e61f05b2b43ba9f9b1c231
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2020-01-05 01:19:55 -0700

    Update fullDev.sh

--- fullDev.sh
+++ fullDev.sh
@@ -12,8 +12,8 @@ pm2 unstartup
 pm2 startup ubuntu -u zero --hp /home/zero
 
 echo -e "\n\n*** Installing MongoDB\n"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
 apt-get install -y mongodb-org
 
