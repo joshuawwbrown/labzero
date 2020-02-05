a66a527cc58c8c4ee43c202abce9c827757201eb
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2020-02-05 02:00:42 -0700

    Update updateNginx.sh

--- updateNginx.sh
+++ updateNginx.sh
@@ -8,9 +8,9 @@ echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ bionic nginx' >> /etc/a
 cat /etc/apt/sources.list.d/nginx.list
 
 wget http://nginx.org/keys/nginx_signing.key
-apt-key add nginx_signing.key
-apt update
-apt remove nginx nginx-common nginx-full nginx-core
+apt-key -y add nginx_signing.key
+apt -y update
+apt -y remove nginx nginx-common nginx-full nginx-core
 
-apt install nginx
+apt -y install nginx
 nginx -v
