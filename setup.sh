ce95b62aa4865941a91fe5caee1b3d15db873b54
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2020-02-05 02:02:56 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,18 @@ yes | ufw enable
 ufw status
 
 echo -e "\n\n*** NGINX\n"
-apt-get install -y nginx
+
+touch /etc/apt/sources.list.d/nginx.list
+echo 'deb [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ bionic nginx' >> /etc/apt/sources.list.d/nginx.list
+echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ bionic nginx' >> /etc/apt/sources.list.d/nginx.list
+
+wget http://nginx.org/keys/nginx_signing.key
+apt-key add nginx_signing.key
+apt -y update
+apt -y remove nginx nginx-common nginx-full nginx-core
+
+apt -y install nginx
+nginx -v
 
 echo -e "\n\n*** AUTO_UPDATES\n"
 apt install -y unattended-upgrades
