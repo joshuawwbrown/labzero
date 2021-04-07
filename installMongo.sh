9309404c68fec4a32ed5e57d30956b2ee603550b
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2021-04-07 04:57:40 -0600

    Update installMongo.sh

--- installMongo.sh
+++ installMongo.sh
@@ -1,19 +1,11 @@
 echo -e "\n\n*** Installing MongoDB 4.2\n"
 
+apt-get install gnupg -y
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update
-apt-get install -y mongodb-org
+apt-get update -y
+apt-get install mongodb-org -y
 
-echo '[Unit]
-Description=MongoDB Storage Engine
-After=network.target
-[Service]
-User=mongodb
-ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
-[Install]
-WantedBy=multi-user.target'\
-> /etc/systemd/system/mongodb.service
-
-systemctl start mongodb.service
-systemctl enable mongodb.service
+systemctl start mongod
+systemctl enable mongod
