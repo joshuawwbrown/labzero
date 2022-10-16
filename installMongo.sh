64d009e2e8503e94a674ffd8b887a072408247e9
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2022-10-16 03:13:06 -0600

    Update installMongo.sh

--- installMongo.sh
+++ installMongo.sh
@@ -1,11 +1,18 @@
-echo -e "\n\n*** Installing MongoDB 4.2\n"
+echo -e "\n\n*** Installing MongoDB 6 for Ubuntu 22\n"
 
-apt-get install gnupg -y
+apt update
+apt install wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt-get update -y
-apt-get install mongodb-org -y
+curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 
-systemctl start mongod
-systemctl enable mongod
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+
+apt update
+apt install mongodb-org
+
+sudo systemctl enable --now mongod
+
+systemctl status mongod
