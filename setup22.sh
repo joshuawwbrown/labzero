fcd5e36be67c0cc2a42b6c2363c3679b0cd1dce7
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2023-10-03 16:44:30 -0600

    Update setup22.sh

--- setup22.sh
+++ setup22.sh
@@ -42,6 +42,16 @@ apt-get install -y git-core;
 apt-get install -y libssl-dev;
 apt-get install -y build-essential
 
+echo -e "\n\n*** NODE JS\n"
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main
+apt-get update
+apt-get install nodejs -y
+
 echo -e "\n\n*** CERTBOT & SSL\n"
 
 cp /root/labzero/timeStamp.sh /root
