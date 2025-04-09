9f492d1932a06f20307f3ebad8d7b631f2b06349
Joshua Brown <joshuawwbrown@users.noreply.github.com>
2025-04-09 14:20:02 -0600

    Create installMongosh8.sh

---
+++ installMongosh8.sh
@@ -0,0 +1,4 @@
+wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-8.0.asc
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
